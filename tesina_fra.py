95c83f8eb4d9be63e70fee11a6c1f0e0a660eeca
Francesco Frassinelli <fraph24@gmail.com>
2024-07-21 22:49:11 +0200

    Port to Python 3 and Qt 6

--- tesina_fra.py
+++ tesina_fra.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # tesina_fra.py
 #
-# Copyright 2009 Francesco Frassinelli <fraph24@gmail.com>
+# Copyright 2024 Francesco Frassinelli <fraph24@gmail.com>
 #
 #     This program is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
@@ -20,50 +20,53 @@
 
 """ Questo programma permette di visualizzare una tesina multidisciplinare,
     permettendo di sfruttare le capacità multimediali del computer.
-    Dipende da QT > 4.5 e PyQt4                                              """
+    Dipende da QT 6 e PyQt6                                                  """
 
-from PyQt4 import QtGui, QtCore, QtWebKit                   # QT 4.5
+from PyQt6 import QtGui, QtCore, QtWidgets, QtWebEngineWidgets # QT 6
 from functools import partial                               # Wrapper funzioni
-from ConfigParser import ConfigParser                       # Parser
+from configparser import ConfigParser                       # Parser
 from sys import argv, exit                                  # Init interfaccia
-from os import sep                                          # Slash portabile
+from os import sep, path                                    # Slash portabile
 
 def get(obj, folder, name, extension):
     """ Ritorna QUrl istanziato con l'indirizzo del file da richiedere """    
-    return obj(folder + sep + name + "." + extension)       # Ritorna l'oggetto
+    return obj(path.abspath(
+            folder + sep + name + "." + extension))         # Ritorna l'oggetto
 
 def get_page(name):
     """ Ritorna QUrl istanziato con l'indirizzo della pagina da richiedere """
-    return get(QtCore.QUrl, "pages", name, "html")          # Ritorna l'URL
+    return get(QtCore.QUrl.fromLocalFile,
+        "pages", name, "html")                              # Ritorna l'URL
 
 def get_icon(name):
     """ Ritorna QIcon istanziato con l'icona da richiedere """
     return get(QtGui.QIcon, "icons", name, "svg")           # Ritorna l'icona
 
-class Browser(QtGui.QWidget):
+class Browser(QtWidgets.QWidget):
     """ Gestisce il widget QtWebKit per la visualizzazione delle pagine """
     def __init__(self, parent = None):
-        QtGui.QWidget.__init__(self, parent)
+        QtWidgets.QWidget.__init__(self, parent)
         self.parent = parent
         
         # Inizializza i widgets
-        self.web = QtWebKit.QWebView()                      # Browser
+        self.web = QtWebEngineWidgets.QWebEngineView()      # Browser
         self.web.load(get_page(self.parent.data[0]))        # Pagina iniziale
-        label = QtGui.QLabel("Applicazione creata da Francesco Frassinelli")
-        label.setAlignment(QtCore.Qt.AlignRight)            # Allineamento
+        label = QtWidgets.QLabel("Applicazione creata da Francesco Frassinelli")
+        label.setAlignment(
+            QtCore.Qt.AlignmentFlag.AlignRight)             # Allineamento
         
         # Imposta il layout
-        layout = QtGui.QGridLayout()                        # Layout verticale
+        layout = QtWidgets.QGridLayout()                    # Layout verticale
         layout.addWidget(self.web, 0, 0)                    # + self.web
         layout.addWidget(label, 1, 0)                       # + crediti
         layout.setRowStretch(0, 100)                        # Rapporto tra >
         layout.setRowStretch(1, 0)                          # > le varie righe
         self.setLayout(layout)                              # Layout
 
-class Tesina(QtGui.QMainWindow):
+class Tesina(QtWidgets.QMainWindow):
     """ Widget per visualizzare la tesina """
     def __init__(self):
-        QtGui.QMainWindow.__init__(self)
+        QtWidgets.QMainWindow.__init__(self)
         
         # Inizializza la finestra
         self.setWindowTitle("Tesina")                       # Titolo
@@ -76,51 +79,52 @@ class Tesina(QtGui.QMainWindow):
         # Leggo la configurazione
         conf = "tesina.conf"                                # Nome del file
         config = ConfigParser()                             # Parser
-        config.readfp(open(conf, "r"))                      # Lettura e parsing
+        config.read(conf)                                   # Lettura e parsing
         self.data = config.get("Indice", "materie").split() # Materie
 
         # Creo la toolbar
-        self.toolbar = QtGui.QToolBar("Materie")            # Creazione
+        self.toolbar = QtWidgets.QToolBar("Materie")        # Creazione
         self.toolbar.setMovable(False)                      # Non spostabile
-        self.addToolBar(QtCore.Qt.LeftToolBarArea,
+        self.addToolBar(QtCore.Qt.ToolBarArea.LeftToolBarArea,
             self.toolbar)                                   # + Toolbar
             
         # Elementi di testa
-        label = QtGui.QLabel("Tesina")                      # Titolo
-        label.setAlignment(QtCore.Qt.AlignCenter)           # Allineamento
+        label = QtWidgets.QLabel("Tesina")                  # Titolo
+        label.setAlignment(
+            QtCore.Qt.AlignmentFlag.AlignCenter)            # Allineamento
         self.bar_add(label)                                 # + Label
         self.toolbar.addSeparator()                         # Separatore
         
         # Materie
         for item in self.data:                              # Per ogni sezione >
             self.generate(item.capitalize(), item,
-                "clicked()", partial(self.see, item))       # > Crea il bottone
+                partial(self.see, item))                    # > Crea il bottone
         
         # Parte finale
-        self.toolbar.addWidget(QtGui.QLabel(""))            # Spazio
+        self.toolbar.addWidget(QtWidgets.QLabel(""))        # Spazio
         self.toolbar.addSeparator()                         # Separatore
-        self.generate("Esci", "exit", "clicked()",
-            QtGui.qApp, QtCore.SLOT("quit()"))              # Bottone esci
+        self.generate("Esci", "exit",
+            QtWidgets.QApplication.instance().quit)         # Bottone esci
         
         # Finalizzazione
         self.browser = Browser(self)                        # Inizializzazione
         self.setCentralWidget(self.browser)                 # Widget principale
     
-    def generate(self, name, icon, signal, *receiver):
+    def generate(self, name, icon, *receiver):
         """ Genera un bottone """
-        style = QtCore.Qt.ToolButtonTextUnderIcon           # Stile di default
-        button = QtGui.QToolButton()                        # Bottone
+        style = QtCore.Qt.ToolButtonStyle.ToolButtonTextUnderIcon
+                                                            # Stile di default
+        button = QtWidgets.QToolButton()                    # Bottone
         button.setText(name)                                # Testo
         button.setIcon(get_icon(icon))                      # Icona
         button.setToolButtonStyle(style)                    # Stile
         button.setFixedWidth(120)                           # Larghezza
-        self.connect(button, QtCore.SIGNAL(signal),
-            *receiver)                                      # Connette
+        button.clicked.connect(*receiver)                   # Connette
         self.bar_add(button)                                # + Bottone
     
     def bar_add(self, widget):
         """ Aggiunge un widget alla toolbar """
-        self.toolbar.addWidget(QtGui.QLabel(""))            # Spazio
+        self.toolbar.addWidget(QtWidgets.QLabel(""))        # Spazio
         self.toolbar.addWidget(widget)                      # + Widget
     
     def see(self, item):
@@ -128,7 +132,7 @@ class Tesina(QtGui.QMainWindow):
         self.browser.web.setUrl(get_page(item))             # Nuova pagina
 
 if __name__ == "__main__":
-    APP = QtGui.QApplication(argv)                          # Inizializza >
+    APP = QtWidgets.QApplication(argv)                      # Inizializza >
     TESINA = Tesina()                                       # > l'applicazione
     TESINA.show()                                           # Mostra la tesina
-    exit(APP.exec_())                                       # Esegue la tesina
+    exit(APP.exec())                                        # Esegue la tesina
